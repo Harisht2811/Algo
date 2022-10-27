4d36a451e6f5bf22073beb76ecfa57374c46ff7b
Harisht2811 <harisht@venzotechnologies.com>
2022-10-27 18:40:25 +0530

    oct 27 18.40

--- src/component/attacksurface/attack.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function attack() {
-  return (
-    <div>attack</div>
-  )
-}
-
-export default attack
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,16 @@
 import * as React from "react"
 
-import Homepage from '../component/homepage/homepage'
+import Header from '../component/homepage/header/header'
 
 const IndexPage = () => {
   return (
-    <div>
+    <>
+       <div>
       <title>Algoritmik</title>
-      <>
-      </>
+     <Header/>
     </div>
+    </>
+ 
    
   )
 }
