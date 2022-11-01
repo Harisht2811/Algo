f0e94454e98baad86aa3465ff3e810b149453439
Harisht2811 <harisht@venzotechnologies.com>
2022-11-01 22:40:35 +0530

    nov 1 22.40

--- src/component/homepage/header/header.js
+++ src/component/homepage/header/header.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import '../header/header.css'
 import { Link } from 'gatsby'
+import { useState } from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 import headerData from '../../../content/headerData.json'
 import algoLogo from '../../../images/homepage/algohome23.png'
-function header() {
+import Toggleicon from '../../../images/homepage/algohome31.svg'
+
+function Header() {
+  const [age, setAge] = useState(' ');
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
   return (
     <>
       <header className='algoheader'>
@@ -20,7 +33,27 @@ function header() {
             }
           </ul>
         </div>
+        <div className='homeServices'>
         <p id='algoheaderText'>Services</p>
+        <img onChange={handleChange} className='toggleicon' src={Toggleicon} alt='toggleicon'></img>
+        <Select  >
+          <div className='selectBg'>
+          <MenuItem >3rd Party Cyber Risk</MenuItem>
+          <hr id='hrinhome'></hr>
+          <MenuItem>Attack Surface</MenuItem>
+          <hr id='hrinhome'></hr>
+          <MenuItem >DDoS Attack Simulation & Testing</MenuItem>
+          <hr id='hrinhome'></hr>
+          <MenuItem >DDOS Incident Response</MenuItem>
+          <hr id='hrinhome'></hr>
+          <MenuItem >Technology Hardening</MenuItem>
+          <hr id='hrinhome'></hr>
+          <MenuItem >Deep & Dark Web</MenuItem>
+          </div>
+          
+        </Select>
+        </div>
+        
       </header>
 
 
@@ -28,4 +61,7 @@ function header() {
   )
 }
 
-export default header
+export default Header
+
+
+
