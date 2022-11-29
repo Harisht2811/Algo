a145013ad282598f6f15eb90ccbfc1f1177a7e91
priyachandrasekar <priyankac@venzotechnologies.com>
2022-11-29 18:39:58 +0530

    changes

--- src/component/contactus/contact.js
+++ src/component/contactus/contact.js
@@ -9,6 +9,114 @@ import './desktop.css'
 
 
 
+// const Contactus = () => {
+
+//     const options = [{ value: '3rd Party Cyber Risk', label: '3rd Party Cyber Risk' },
+//     { value: 'Attack Surface', label: 'Attack Surface' },
+//     { value: 'DDoS Attack Simulation & Testing', label: 'DDoS Attack Simulation & Testing' },
+//     { value: 'DDOS Incident Response', label: 'DDOS Incident Response' },
+//     { value: 'Technology Hardening', label: 'Technology Hardening' },
+//     { value: 'Deep & Dark Web', label: 'Deep & Dark Web' },
+//     { value: 'Deep Cyber Investigations', label: 'Deep Cyber Investigations' },
+//     { value: 'Pentesting', label: 'Pentesting' },
+//     { value: 'Phishing Detection', label: 'Phishing Detection' },
+//     { value: 'Virtual Humint Operations', label: 'Virtual Humint Operations' },
+//     { value: 'Ransomeware', label: 'Ransomeware' }
+//     ]
+
+//     return (
+//         <div>
+//             <div className='contactBanner'>
+//                 <p className='contactTitle1'>Get in touch with us</p>
+//                 <p className='contactTitle2'>We will help you in testing and cyber security issues</p>
+//                 <div className='ContactFlex'>
+//                     <div className='contactForm'>
+//                         <form className='form'>
+//                             <div className='formContent1'>
+//                                 <p className='label'>
+//                                     <label>Your Name*</label>
+//                                     <input type='text' className='input1'></input>
+//                                 </p>
+//                                 <p className='label'>
+//                                     <label className='email'>Your Email*</label>
+//                                     <input type='email' className='input2' ></input>
+//                                 </p>
+//                             </div>
+//                             <div className='formContent'>
+//                                 <p className='label'>
+//                                     <label>Company Name</label>
+//                                     <input type='text' className='input3'></input>
+//                                 </p>
+//                             </div>
+//                             <div className='formContent'>
+//                                 <p className='label'>
+//                                     <label className='serviceNeed'>Services You Need?</label>
+//                                     <div className='selectValue'>
+//                                         <Select options={options} isMulti className='ContactSelect' placeholder='sevices' />
+//                                     </div>
+//                                 </p>
+//                             </div>
+//                             <div className='formContent3'>
+//                                 <p className='label'>
+//                                     <label>How Can We Help You?</label><br></br>
+//                                     <input type='text' className='inputMsg' ></input>
+//                                 </p>
+//                             </div>
+//                             <button className='submitBtn'><p className='submit'>Submit</p></button>
+//                         </form>
+//                     </div>
+//                     <div className='ContactPara'>
+//                         <p className='ContactParaTitle1'>World-Class Cyber-Security services & Testings</p>
+//                         <p className='paraContent'>Providing cyber security advice & services over 150+ world countries. With top analysis & execution we are proud to be the one of the best cyber security service provider in USA.</p>
+//                         <hr className='ContactHr'></hr>
+//                         <p className='ContactParaTitle2'>Looking for an expert advice ?</p>
+//                         <p className='paraContent2'>We are here to help. Email us your needs in a brief to</p>
+//                         <p className='linkCont'>kelly@algoritmik.com</p>
+//                     </div>
+//                 </div>
+//                 <div className='meetourTeam'>
+//                     <p id='ourteamText'>Meet our team</p>
+//                     <div className='ourteamProfile'>
+//                         <div>
+//                             <div className='profile1'>
+//                             </div>
+//                             <p id='nameText'>Andrew </p>
+//                             <p id='speccialist'>Head of Cyber Attack</p>
+//                         </div>
+
+//                         <div>
+//                             <div className='profile2'>
+//                             </div>
+//                             <p id='nameText'>Simon </p>
+//                             <p id='speccialist'>DDOS Expert</p>
+//                         </div>
+//                         <div>
+//                             <div className='profile3'>
+//                             </div>
+//                             <p id='nameText'>Edwin </p>
+//                             <p id='speccialist'>Phishing Expert</p>
+//                         </div>
+//                         <div>
+//                             <div className='profile4'>
+//                             </div>
+//                             <p id='nameText'>Melina </p>
+//                             <p id='speccialist'>Pentesting Head</p>
+//                         </div>
+
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+
+
+// export default Contactus
+
+
+
+import contactData from '../../content/contactData.json'
+
 const Contactus = () => {
 
     const options = [{ value: '3rd Party Cyber Risk', label: '3rd Party Cyber Risk' },
@@ -25,10 +133,13 @@ const Contactus = () => {
     ]
 
     return (
+
         <div>
+            {contactData.map (item => {
+                return <div> 
             <div className='contactBanner'>
-                <p className='contactTitle1'>Get in touch with us</p>
-                <p className='contactTitle2'>We will help you in testing and cyber security issues</p>
+                <p className='contactTitle1'>{item.title}</p>
+                <p className='contactTitle2'>{item.discription}</p>
                 <div className='ContactFlex'>
                     <div className='contactForm'>
                         <form className='form'>
@@ -66,12 +177,12 @@ const Contactus = () => {
                         </form>
                     </div>
                     <div className='ContactPara'>
-                        <p className='ContactParaTitle1'>World-Class Cyber-Security services & Testings</p>
-                        <p className='paraContent'>Providing cyber security advice & services over 150+ world countries. With top analysis & execution we are proud to be the one of the best cyber security service provider in USA.</p>
+                        <p className='ContactParaTitle1'>{item.wordTitle}</p>
+                        <p className='paraContent'>{item.wordContent}</p>
                         <hr className='ContactHr'></hr>
-                        <p className='ContactParaTitle2'>Looking for an expert advice ?</p>
-                        <p className='paraContent2'>We are here to help. Email us your needs in a brief to</p>
-                        <p className='linkCont'>kelly@algoritmik.com</p>
+                        <p className='ContactParaTitle2'>{item.wordTitle2}</p>
+                        <p className='paraContent2'>{item.wordContent2}</p>
+                        <p className='linkCont'>{item.Email}</p>
                     </div>
                 </div>
                 <div className='meetourTeam'>
@@ -106,6 +217,8 @@ const Contactus = () => {
                     </div>
                 </div>
             </div>
+            </div>
+            })}
         </div>
     )
 }
--- src/component/ddoscyber/ddoscyber.js
+++ src/component/ddoscyber/ddoscyber.js
@@ -18,60 +18,129 @@ import './mob.css'
 import './mobile.css'
 import './monitor.css'
 
+// function Ddoscyber() {
+//   const [buttonPopup, setButtonPopup] =useState(false);
+//   return (
+//     <>
+//     <div>
+//     <div>
+//     <div className='cyberBannerImg'>
+//         <p className='cyberTitle1'><span className='colorText'>Get the Intelligence</span> You Need,<br></br>When You Need It</p>
+//           <div className='cyberTitleFlex'>
+//              <div className='Arrow'>
+//               <img src={Arrow} alt='arrow'></img>
+//               <p id='arrow'>Strategic <br></br>Threat Intelligence</p>
+//               </div>
+//               <div className='Tick'>
+//                 <img src={Tick} alt='tick'></img>
+//                 <p id='tick'>Threat <br></br> Actor Profiling</p>
+//               </div>
+//               <div className='Hexagon'>
+//                 <img src={Hexagon} alt='Hexagon'></img>
+//                 <p id='hexagon'>Threat<br></br> Landscape Reports</p>
+//               </div>
+//           </div>
+//       </div>
+//       <div className='cyberBanner2'>
+//         <p className='cyberBanner2Par'>Most businesses and organizations don’t have the tools or time to keep up with escalating and increasingly sophisticated threats to their physical and digital assets. With Algoritmik’s Deep Dive Investigations, you can assess and respond to these threats with the help of our highly skilled analysts to build cybersecurity analysis, threat assessments, analytic projects and deep-dive reports, all customized to your needs</p>
+//       </div>
+//     </div>
+      
+//       <div className='cardsCyber'>
+//       <div className='cyberCards'>
+//           <div className='strategicCard1'>
+//             <div className='strategicPara'> 
+//               <p className='strategicCard1Title'>Strategic <br></br> Threat Intelligence</p>
+//               <p className='card1Para'>Identify meaningful trends to gain a wider perspective by continuously analyzing threat data. Create strategic threat intelligence and security advisory reports that provide the basis for the formulation of strategy, policy, and longer-term decision-making</p>
+//               <p id='demo1'>Enquire Now <img src={arrow} className='demoArrow' alt='arrow' onClick={() => setButtonPopup(true)}/></p>
+//             </div>
+//             <img src={Rocket} className='cardImg' alt='rocket'></img>
+//           </div>
+//           <div className='threatCard2'>
+//             <div className='threatPara'>
+//               <p className='ThreatActorTitle'>Threat <br></br>Actor Profiling</p>
+//               <p className='card2Para'>Achieve deeper understanding of a specific threat actor and group profiling, including the place of operations, targeted countries or verticals, tools in use and the type of operations often associated with those groups</p>
+//               <p id='demo1'>Enquire Now <img src={arrow} className='demoArrow' alt='arrow' onClick={() => setButtonPopup(true)}/></p>
+//             </div>
+//             <img src={ThreatActor} className='cardImg' alt='threatactor'></img>
+//           </div>
+//           <div className='threatCard3'>
+//             <div className='threatcar3Para'> 
+//               <p className='threatCard3Title'>Threat <br></br>Landscape Reports</p>
+//               <p className='card3Para'>Gain a more comprehensive view of the cyber threat landscape with reports focused on specific verticals that deliver an overview of the current situation and its relation to your industry</p>
+//               <p id='demo1'>Enquire Now <img src={arrow} className='demoArrow' alt='arrow' onClick={() => setButtonPopup(true)}/></p>
+//             </div>
+//             <img src={ThreatLandscape} className='cardImg' alt='threatlandscape'></img>
+//           </div>
+//         </div>
+//         <div className='heard'>
+//             <div className='securityIssues'>
+//                 <div className='securityText'>
+//                 <p id='titleIssues'>Heard enough? See it for yourself.</p>
+//                 <p id='textIssues'>Security issues that the penetration test uncovers should be reported to the system owner. Penetration test reports may also assess potential impacts to the organization.</p>
+//                 </div>
+//                 <button className='certificateButton' onClick={() => setButtonPopup(true)}>Enquire Now</button>
+//             </div>
+//         </div>
+//       </div>
+      
+      
+//     </div>
+//     <Popup trigger ={buttonPopup} setTrigger ={setButtonPopup}>
+//      <p id='getintouch'>Get in touch with us </p>
+//      <label>Name*</label>
+//      <input className='Fname' type="text" ></input>
+//      <label>Email*</label>
+//      <input className='Femail' type="text" ></input>
+//      <label>Mobile Number*</label>
+//      <input className='Fphone' type="phone" ></input>
+//      <button className='Fbutton'>Submit</button>    
+//     </Popup>
+//         </>
+//   )
+// }
+
+// export default Ddoscyber
+
+import cyberData from '../../content/cyberData.json'
 function Ddoscyber() {
   const [buttonPopup, setButtonPopup] =useState(false);
   return (
     <>
     <div>
+      {cyberData.map (item => {
+        return <div> 
     <div>
     <div className='cyberBannerImg'>
-        <p className='cyberTitle1'><span className='colorText'>Get the Intelligence</span> You Need,<br></br>When You Need It</p>
+        <p className='cyberTitle1'><span className='colorText'>{item.cyberTitle1}</span>{item.cyberTitle2}<br></br>{item.cyberTitle3}</p>
           <div className='cyberTitleFlex'>
-             <div className='Arrow'>
-              <img src={Arrow} alt='arrow'></img>
-              <p id='arrow'>Strategic <br></br>Threat Intelligence</p>
+         {item.flex.map (item => {
+          return <div className='Arrow'>
+              <img src={item.log} alt='arrow' className='arrowLogo'></img>
+              <p id='arrow'>{item.iconTitle}<br></br>{item.iconTit}</p>
               </div>
-              <div className='Tick'>
-                <img src={Tick} alt='tick'></img>
-                <p id='tick'>Threat <br></br> Actor Profiling</p>
-              </div>
-              <div className='Hexagon'>
-                <img src={Hexagon} alt='Hexagon'></img>
-                <p id='hexagon'>Threat<br></br> Landscape Reports</p>
-              </div>
+            })}
           </div>
       </div>
       <div className='cyberBanner2'>
-        <p className='cyberBanner2Par'>Most businesses and organizations don’t have the tools or time to keep up with escalating and increasingly sophisticated threats to their physical and digital assets. With Algoritmik’s Deep Dive Investigations, you can assess and respond to these threats with the help of our highly skilled analysts to build cybersecurity analysis, threat assessments, analytic projects and deep-dive reports, all customized to your needs</p>
+        <p className='cyberBanner2Par'>{item.cyberPara}</p>
       </div>
     </div>
       
       <div className='cardsCyber'>
-      <div className='cyberCards'>
-          <div className='strategicCard1'>
-            <div className='strategicPara'> 
-              <p className='strategicCard1Title'>Strategic <br></br> Threat Intelligence</p>
-              <p className='card1Para'>Identify meaningful trends to gain a wider perspective by continuously analyzing threat data. Create strategic threat intelligence and security advisory reports that provide the basis for the formulation of strategy, policy, and longer-term decision-making</p>
-              <p id='demo1'>Enquire Now <img src={arrow} className='demoArrow' alt='arrow' onClick={() => setButtonPopup(true)}/></p>
+       <div className='cyberCards'>
+       {item.cyberCard.map (item => {
+      return<div className='strategicCard1'>
+
+           <div className='strategicPara'> 
+              <p className='strategicCard1Title'>{item.cardTitle}<br></br>{item.cardTit}</p>
+              <p className='card1Para'>{item.cardPara}</p>
+              <p id='demo1'>{item.cardcont}<img src={item.cardContImg} className='demoArrow' alt='arrow' onClick={() => setButtonPopup(true)}/></p>
             </div>
-            <img src={Rocket} className='cardImg' alt='rocket'></img>
+            <img src={item.cardImg} className='cardImg' alt='rocket'></img>
+
           </div>
-          <div className='threatCard2'>
-            <div className='threatPara'>
-              <p className='ThreatActorTitle'>Threat <br></br>Actor Profiling</p>
-              <p className='card2Para'>Achieve deeper understanding of a specific threat actor and group profiling, including the place of operations, targeted countries or verticals, tools in use and the type of operations often associated with those groups</p>
-              <p id='demo1'>Enquire Now <img src={arrow} className='demoArrow' alt='arrow' onClick={() => setButtonPopup(true)}/></p>
-            </div>
-            <img src={ThreatActor} className='cardImg' alt='threatactor'></img>
-          </div>
-          <div className='threatCard3'>
-            <div className='threatcar3Para'> 
-              <p className='threatCard3Title'>Threat <br></br>Landscape Reports</p>
-              <p className='card3Para'>Gain a more comprehensive view of the cyber threat landscape with reports focused on specific verticals that deliver an overview of the current situation and its relation to your industry</p>
-              <p id='demo1'>Enquire Now <img src={arrow} className='demoArrow' alt='arrow' onClick={() => setButtonPopup(true)}/></p>
-            </div>
-            <img src={ThreatLandscape} className='cardImg' alt='threatlandscape'></img>
-          </div>
+        })}
         </div>
         <div className='heard'>
             <div className='securityIssues'>
@@ -84,7 +153,8 @@ function Ddoscyber() {
         </div>
       </div>
       
-      
+      </div>
+      })}
     </div>
     <Popup trigger ={buttonPopup} setTrigger ={setButtonPopup}>
      <p id='getintouch'>Get in touch with us </p>
